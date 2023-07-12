352bd095073846f8900b1130d28089e40b92a242
kalathen <kalathen@jyu.fi>
2023-07-12 16:39:57 +0200

    tests for non cache rules

--- src/test/lib/optimizer/strategy/strategy_base_test.cpp
+++ src/test/lib/optimizer/strategy/strategy_base_test.cpp
@@ -7,6 +7,7 @@
 #include "cost_estimation/cost_estimator_logical.hpp"
 #include "logical_query_plan/abstract_lqp_node.hpp"
 #include "logical_query_plan/logical_plan_root_node.hpp"
+#include "optimizer/optimizer.hpp"
 #include "optimizer/strategy/abstract_rule.hpp"
 #include "statistics/cardinality_estimator.hpp"
 
@@ -14,6 +15,14 @@ namespace hyrise {
 
 std::shared_ptr<AbstractLQPNode> StrategyBaseTest::apply_rule(const std::shared_ptr<AbstractRule>& rule,
                                                               const std::shared_ptr<AbstractLQPNode>& input) {
+  const auto lqp_result = apply_rule_with_cache(rule, input);
+  const auto lqp = lqp_result.logical_query_plan;
+
+  return lqp;
+}
+
+OptimizedLogicalQueryPlan StrategyBaseTest::apply_rule_with_cache(const std::shared_ptr<AbstractRule>& rule,
+                                                                  const std::shared_ptr<AbstractLQPNode>& input) {
   // Add explicit root node
   const auto root_node = LogicalPlanRootNode::make();
   root_node->set_left_input(input);
@@ -23,13 +32,13 @@ std::shared_ptr<AbstractLQPNode> StrategyBaseTest::apply_rule(const std::shared_
   const auto cost_estimator = std::make_shared<CostEstimatorLogical>(cardinality_estimator);
   rule->cost_estimator = cost_estimator;
 
-  rule->apply_to_plan(root_node);
+  const auto cacheable = rule->apply_to_plan(root_node);
 
   // Remove LogicalPlanRootNode
   const auto optimized_node = root_node->left_input();
   root_node->set_left_input(nullptr);
 
-  return optimized_node;
+  return {static_cast<bool>(cacheable), optimized_node};
 }
 
 }  // namespace hyrise
